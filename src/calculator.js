cedf5dcd53e5414b3048312d6862c8da4bc62117
Caruki <caruki91@posteo.de>
2020-02-26 10:40:30 +0100

    Try javascript on calc
    Add basic math functions

--- src/calculator.js
+++ src/calculator.js
@@ -1,1 +1,16 @@
-document.querySelector(".calculator__output").value = "80";
+"use strict";
+
+const calculatorOutput = document.querySelector(".calculator__output");
+calculatorOutput.value = multiplicate(4, 6);
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+
+function multiplicate(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
