38a03b8e13ceb259e6e1651af54bc087a459bad9
Caruki <caruki91@posteo.de>
2020-02-26 14:29:14 +0100

    Add button function for result and clear

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,9 @@
 "use strict";
 
 const calculatorOutput = document.querySelector(".calc-output");
-const calculatorInput = document.querySelector(".calc-input");
+const calculatorInputs = document.querySelectorAll(".calc-input");
+const calculatorResult = document.querySelector(".calc-result");
+const calculatorClear = document.querySelector(".calc-clear");
 
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
@@ -19,5 +21,18 @@ function divide(numberOne, numberTwo) {
   return numberOne / numberTwo;
 }
 
-const NumberOne = Number(calculatorInput.innerText);
-calculatorOutput.value = add(NumberOne, 33);
+const numberOne = Number(calculatorInputs[3].innerText);
+const numberTwo = Number(calculatorInputs[9].innerText);
+
+function clickResult() {
+  calculatorOutput.value = add(numberOne, numberTwo);
+  console.log("clicked");
+}
+
+function clickClear() {
+  calculatorOutput.value = "";
+  console.log("clicked");
+}
+
+calculatorResult.addEventListener("click", clickResult);
+calculatorClear.addEventListener("click", clickClear);
