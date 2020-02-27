2be9598ff6e31a76c8f7230f3dfde5010e1d7982
Caruki <caruki91@posteo.de>
2020-02-27 12:03:32 +0100

    Add calculate function to account for different operators
    Add function to maths module

--- src/math.js
+++ src/math.js
@@ -13,3 +13,22 @@ export function multiplicate(numberOne, numberTwo) {
 export function divide(numberOne, numberTwo) {
   return numberOne / numberTwo;
 }
+
+export function calculate(numberOne, numberTwo, operator) {
+  switch (operator) {
+    case "+":
+      return add(numberOne, numberTwo);
+
+    case "-":
+      return subtract(numberOne, numberTwo);
+
+    case "*":
+      return multiplicate(numberOne, numberTwo);
+
+    case "/":
+      return divide(numberOne, numberTwo);
+
+    default:
+      alert("Incorrect value");
+  }
+}
